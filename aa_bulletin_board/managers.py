92902cc4af6ce8f7e9d5c812eef602b333758ce0
Peter Pfeufer <info@ppfeufer.de>
2021-08-25 11:08:19 +0200

    add group restrictions to bulletins

---
+++ aa_bulletin_board/managers.py
@@ -0,0 +1,34 @@
+"""
+Managers for our models
+"""
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Q
+
+
+class BulletinQuerySet(models.QuerySet):
+    def user_has_access(self, user: User) -> models.QuerySet:
+        """
+        Filter boards that given user has access to.
+        """
+
+        # Forum manager have always access, so assign this permission wisely
+        if user.has_perm("aa_bulletin_board.manage_bulletins"):
+            return self
+
+        # If not a forum manager, check if the user has access to the board
+        return self.filter(
+            Q(groups__in=user.groups.all()) | Q(groups__isnull=True)
+        ).distinct()
+
+
+class BulletinManagerBase(models.Manager):
+    """
+    BoardManagerBase
+    """
+
+    pass
+
+
+BulletinManager = BulletinManagerBase.from_queryset(BulletinQuerySet)
