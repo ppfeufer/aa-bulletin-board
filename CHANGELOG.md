39ce8fdb079b076b81038645c83786e86c411bfe
Peter Pfeufer <info@ppfeufer.de>
2022-02-02 10:47:10 +0100

    Using `path` in URL config instead of soon-to-be removed `url`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 
+## [In Develoment] - Unreleased
+
+### Changed
+
+- Using `path` in URL config instead of soon-to-be removed `url`
+
+
 ## [1.2.0] - 2022-01-19
 
 ### Added
--- aa_bulletin_board/urls.py
+++ aa_bulletin_board/urls.py
@@ -13,7 +13,7 @@ app_name: str = "aa_bulletin_board"
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("create/", views.create_bulletin, name="create_bulletin"),
-    path("bulletin/<str:slug>/", views.view_bulletin, name="view_bulletin"),
-    path("edit/<str:slug>/", views.edit_bulletin, name="edit_bulletin"),
-    path("remove/<str:slug>/", views.remove_bulletin, name="remove_bulletin"),
+    path("bulletin/<slug:slug>/", views.view_bulletin, name="view_bulletin"),
+    path("edit/<slug:slug>/", views.edit_bulletin, name="edit_bulletin"),
+    path("remove/<slug:slug>/", views.remove_bulletin, name="remove_bulletin"),
 ]
