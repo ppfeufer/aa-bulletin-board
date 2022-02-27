ad694b06414e10d09876cf8c329bc8f90e184511
Peter Pfeufer <info@ppfeufer.de>
2022-02-27 16:03:47 +0100

    [FIX] cannot import name 'ugettext_lazy' from 'django.utils.translation'

--- aa_bulletin_board/forms.py
+++ aa_bulletin_board/forms.py
@@ -9,7 +9,7 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib.auth.models import Group
 from django.forms import ModelForm
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # AA Bulletin Board
 from aa_bulletin_board.models import Bulletin
