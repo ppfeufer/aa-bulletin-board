989da5bc4cbd6a9730a4a4f398855b3cef1df72a
Peter Pfeufer <info@ppfeufer.de>
2023-06-23 16:42:42 +0200

    [ADD] Model translations

--- aa_bulletin_board/apps.py
+++ aa_bulletin_board/apps.py
@@ -4,6 +4,7 @@ App config
 
 # Django
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 # AA Bulletin Board
 from aa_bulletin_board import __version__
@@ -16,4 +17,5 @@ class AaBulletinBoardConfig(AppConfig):
 
     name = "aa_bulletin_board"
     label = "aa_bulletin_board"
-    verbose_name = f"Bulletin Board v{__version__}"
+    # Translators: This is the app name and version, which will appear in the Django Backend
+    verbose_name = _(f"Bulletin Board v{__version__}")
