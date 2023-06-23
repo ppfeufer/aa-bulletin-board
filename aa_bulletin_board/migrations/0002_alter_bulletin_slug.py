9e96ac09370fbda07f4c61a20722cfa55f23a830
Peter Pfeufer <info@ppfeufer.de>
2023-06-23 16:20:50 +0200

    [MISC] pre-commit config updated and applied

--- aa_bulletin_board/migrations/0002_alter_bulletin_slug.py
+++ aa_bulletin_board/migrations/0002_alter_bulletin_slug.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("aa_bulletin_board", "0001_initial"),
     ]
--- aa_bulletin_board/migrations/0003_group_restrictions.py
+++ aa_bulletin_board/migrations/0003_group_restrictions.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("auth", "0012_alter_user_first_name_max_length"),
         ("aa_bulletin_board", "0002_alter_bulletin_slug"),
