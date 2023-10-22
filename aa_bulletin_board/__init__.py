8c73575198e86c00b4cc359d7b9b05033f5b2138
Peter Pfeufer <info@ppfeufer.de>
2023-10-22 12:50:19 +0200

    [CHANGE] Move version to `__init__.py`

--- aa_bulletin_board/__init__.py
+++ aa_bulletin_board/__init__.py
@@ -2,13 +2,8 @@
 App init
 """
 
-# Standard Library
-from importlib import metadata
-
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = metadata.version(distribution_name="aa-bulletin-board")
+__version__ = "1.13.2"
 __title__ = _("Bulletin Board")
-
-del metadata
