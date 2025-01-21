17f644f3ba5c27c4e9dd5e69eb5b0c9420d69607
Peter Pfeufer <info@ppfeufer.de>
2025-01-21 05:04:51 +0100

    [ADD] SRI hashes to static files

---
+++ aa_bulletin_board/constants.py
@@ -0,0 +1,11 @@
+"""
+Constants used in this app
+"""
+
+# Standard Library
+import os
+
+AA_BULLETIN_BOARD_BASE_DIR = os.path.join(os.path.dirname(__file__))
+AA_BULLETIN_BOARD_STATIC_DIR = os.path.join(
+    AA_BULLETIN_BOARD_BASE_DIR, "static", "aa_bulletin_board"
+)
--- aa_bulletin_board/tests/test_templatetags.py
+++ aa_bulletin_board/tests/test_templatetags.py
@@ -4,10 +4,11 @@ Test the apps' template tags
 
 # Django
 from django.template import Context, Template
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 # AA Bulletin Board
 from aa_bulletin_board import __version__
+from aa_bulletin_board.helper.static_files import calculate_integrity_hash
 
 
 class TestVersionedStatic(TestCase):
@@ -15,9 +16,10 @@ class TestVersionedStatic(TestCase):
     Test aa_bulletin_board_static template tag
     """
 
-    def test_versioned_static(self) -> None:
+    @override_settings(DEBUG=False)
+    def test_versioned_static_without_debug_enabled(self) -> None:
         """
-        Test versioned static template tag
+        Test versioned static template tag without DEBUG enabled
 
         :return:
         :rtype:
@@ -27,13 +29,51 @@ class TestVersionedStatic(TestCase):
         template_to_render = Template(
             template_string=(
                 "{% load aa_bulletin_board %}"
-                "{% aa_bulletin_board_static 'aa_bulletin_board/css/aa-bulletin-board.min.css' %}"  # pylint: disable=line-too-long
+                "{% aa_bulletin_board_static 'css/aa-bulletin-board.min.css' %}"
+                "{% aa_bulletin_board_static 'javascript/aa-bulletin-board-oembed.min.js' %}"
             )
         )
 
         rendered_template = template_to_render.render(context=context)
 
-        self.assertInHTML(
-            needle=f'/static/aa_bulletin_board/css/aa-bulletin-board.min.css?v={context["version"]}',  # pylint: disable=line-too-long
-            haystack=rendered_template,
+        expected_static_css_src = f'/static/aa_bulletin_board/css/aa-bulletin-board.min.css?v={context["version"]}'
+        expected_static_css_src_integrity = calculate_integrity_hash(
+            "css/aa-bulletin-board.min.css"
         )
+        expected_static_js_src = f'/static/aa_bulletin_board/javascript/aa-bulletin-board-oembed.min.js?v={context["version"]}'
+        expected_static_js_src_integrity = calculate_integrity_hash(
+            "javascript/aa-bulletin-board-oembed.min.js"
+        )
+
+        self.assertIn(member=expected_static_css_src, container=rendered_template)
+        self.assertIn(
+            member=expected_static_css_src_integrity, container=rendered_template
+        )
+        self.assertIn(member=expected_static_js_src, container=rendered_template)
+        self.assertIn(
+            member=expected_static_js_src_integrity, container=rendered_template
+        )
+
+    @override_settings(DEBUG=True)
+    def test_versioned_static_with_debug_enabled(self) -> None:
+        """
+        Test versioned static template tag with DEBUG enabled
+
+        :return:
+        :rtype:
+        """
+
+        context = Context({"version": __version__})
+        template_to_render = Template(
+            template_string=(
+                "{% load aa_bulletin_board %}"
+                "{% aa_bulletin_board_static 'css/aa-bulletin-board.min.css' %}"
+            )
+        )
+
+        rendered_template = template_to_render.render(context=context)
+
+        expected_static_css_src = f'/static/aa_bulletin_board/css/aa-bulletin-board.min.css?v={context["version"]}'
+
+        self.assertIn(member=expected_static_css_src, container=rendered_template)
+        self.assertNotIn(member="integrity=", container=rendered_template)
