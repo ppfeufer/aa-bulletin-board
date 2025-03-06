6459b20a8a109cf61fe6e27244c67153290fdd59
Peter Pfeufer <info@ppfeufer.de>
2025-03-06 13:19:55 +0100

    [CHANGE] Templatetag code improved

--- aa_bulletin_board/helper/static_files.py
+++ aa_bulletin_board/helper/static_files.py
@@ -25,15 +25,18 @@ logger = LoggerAddTag(my_logger=get_extension_logger(__name__), prefix=__title__
 def calculate_integrity_hash(relative_file_path: str) -> str:
     """
     Calculates the integrity hash for a given static file
+
     :param self:
     :type self:
-    :param relative_file_path: The file path relative to the `aa-timezones/timezones/static/timezones` folder
+    :param relative_file_path: The file path relative to the `{APP_NAME}/{PACKAGE_NAME}/static/{PACKAGE_NAME}` folder
     :type relative_file_path: str
     :return: The integrity hash
     :rtype: str
     """
 
     file_path = os.path.join(APP_STATIC_DIR, relative_file_path)
-    integrity_hash = calculate_integrity(Path(file_path), Algorithm.SHA512)
+    integrity_hash = calculate_integrity(
+        path=Path(file_path), algorithm=Algorithm.SHA512
+    )
 
     return integrity_hash
--- aa_bulletin_board/templatetags/aa_bulletin_board.py
+++ aa_bulletin_board/templatetags/aa_bulletin_board.py
@@ -19,18 +19,23 @@ from app_utils.logging import LoggerAddTag
 
 # AA Bulletin Board
 from aa_bulletin_board import __title__, __version__
+from aa_bulletin_board.constants import PACKAGE_NAME
 from aa_bulletin_board.helper.static_files import calculate_integrity_hash
 
 logger = LoggerAddTag(my_logger=get_extension_logger(__name__), prefix=__title__)
 
 
 @register.simple_tag
-def aa_bulletin_board_static(relative_file_path: str) -> str | None:
+def aa_bulletin_board_static(
+    relative_file_path: str, script_type: str = None
+) -> str | None:
     """
     Versioned static URL
 
-    :param relative_file_path: The file path relative to the `aa-aa_bulletin_board/aa_bulletin_board/static/aa_bulletin_board` folder
+    :param relative_file_path: The file path relative to the `{APP_NAME}/{PACKAGE_NAME}/static/{PACKAGE_NAME}` folder
     :type relative_file_path: str
+    :param script_type: The script type
+    :type script_type: str
     :return: Versioned static URL
     :rtype: str
     """
@@ -45,7 +50,7 @@ def aa_bulletin_board_static(relative_file_path: str) -> str | None:
     if file_type not in ["css", "js"]:
         raise ValueError(f"Unsupported file type: {file_type}")
 
-    static_file_path = os.path.join("aa_bulletin_board", relative_file_path)
+    static_file_path = os.path.join(PACKAGE_NAME, relative_file_path)
     static_url = static(static_file_path)
 
     # Integrity hash calculation only for non-debug mode
@@ -64,12 +69,20 @@ def aa_bulletin_board_static(relative_file_path: str) -> str | None:
         else static_url + "?v=" + __version__
     )
 
+    return_value = None
+
     # Return the versioned URL with integrity hash for CSS
     if file_type == "css":
-        return mark_safe(f'<link rel="stylesheet" href="{versioned_url}"{sri_string}>')
+        return_value = mark_safe(
+            f'<link rel="stylesheet" href="{versioned_url}"{sri_string}>'
+        )
 
     # Return the versioned URL with integrity hash for JS files
     if file_type == "js":
-        return mark_safe(f'<script src="{versioned_url}"{sri_string}></script>')
+        js_type = f' type="{script_type}"' if script_type else ""
 
-    return None  # pragma: no cover
+        return_value = mark_safe(
+            f'<script{js_type} src="{versioned_url}"{sri_string}></script>'
+        )
+
+    return return_value
